6863e98252c92748b0e78a169a9b22ba9e899531
Jonas Baumann <jone@jone.ch>
2011-12-02 11:41:11 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -6,30 +6,40 @@ maintainer = 'Elio Schmutz'
 
 setup(name='collective.js.ui.multiselect',
       version=version,
-      description="turning html multiple select inputs into a sexier equivalent",
-      long_description=open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read(),
+      description='Integrates the multiselect widget into plone.',
+      long_description=open('README.txt').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         ],
-      keywords='jquery ui multiselect',
-      author='%s, 4teamwork GmbH' % maintainer,
+
+      keywords='jquery ui multiselect plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
+      url='https://github.com/4teamwork/collective.js.ui.multiselect',
       license='GPL2',
-      url='http://www.quasipartikel.at/multiselect/',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.js', 'collective.js.ui'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
         'setuptools',
-        # -*- Extra requirements: -*-
         ],
-      entry_points="""
+
+      entry_points='''
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
-      """,
+      ''',
       )
